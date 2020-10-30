9d3a0812627333d4fc8f147a32e1532836ddb919
Philipp Muens <raute1337@gmx.de>
2020-10-30 13:38:34 +0100

    Implement inc

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -28,6 +28,11 @@ mod incrementer {
         pub fn get(&self) -> i32 {
             self.value
         }
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
     }
 
     #[cfg(test)]
@@ -38,6 +43,16 @@ mod incrementer {
         fn default_works() {
             let contract = Incrementer::default();
             assert_eq!(contract.get(), 0);
+        }
+
+        #[test]
+        fn it_works() {
+            let mut contract = Incrementer::new(42);
+            assert_eq!(contract.get(), 42);
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
         }
     }
 }
